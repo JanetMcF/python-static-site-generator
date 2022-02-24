0e34a9894c8c50cebc88a0526e9766fe18204c12
JanetMcF <82966128+JanetMcF@users.noreply.github.com>
2022-02-24 08:02:23 -0700

    Modified

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -8,37 +8,28 @@ class Parser:
     extensions: List[str] = []
 
     def valid_extension(self, extension):
-        if self.extensions.__contains__(extension):
-            return True
-        else:
-            return False
+        return extension in self.extensions
 
-    @classmethod
-    def parse(cls, path: Path, source: Path, dest: Path):
-            raise NotImplementedError
+    def parse(self, path: Path, source: Path, dest: Path):
+        raise NotImplementedError
 
-    @classmethod
-    def read(cls, path):
+    def read(self, path):
         with open(path, 'r') as file:
-            file.read()
+            return file.read()
 
-    @classmethod
-    def write(cls, path, dest, content, ext=".html"):
+    def write(self, path, dest, content, ext=".html"):
         full_path = dest / path.with_suffix(ext).name
         with open(full_path, 'w') as file:
             file.write(content)
 
-    @classmethod
-    def copy(cls, path, source, dest):
-        shutil.copy2(path, dest / source.path)
+    def copy(self, path, source, dest):
+        shutil.copy2(path, dest / path.relative_to(source))
 
 
 class ResourceParser(Parser):
 
     extensions = [".jpg", ".png", ".gif", ".css", ".html"]
 
-    def parse(self, path: Path, source: Path, dest: Path):
-        pass
+    def parse(self, path, source, dest):
+       self.copy(path, source, dest)
 
-    def copy(self, path, source, dest):
-        pass
